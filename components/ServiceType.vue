50f2f3442748e2adeac0c41926af40f72b62d584
Christoph Friedrich <christoph@friedrich-whv.de>
2025-04-17 15:03:57 +0200

    Fix typo

--- components/ServiceType.vue
+++ components/ServiceType.vue
@@ -14,7 +14,7 @@
 
 		<slot name="before-description" v-bind="$props"></slot>
 
-		<section class="description" v-if="service.description || service.deprecated || service.experimental || serivce['federation:backends']">
+		<section class="description" v-if="service.description || service.deprecated || service.experimental || service['federation:backends']">
 			<h3>Description</h3>
 			<Description v-if="service.description" :description="service.description" />
 			<DeprecationNotice v-if="service.deprecated" entity="type of secondary web service" />
