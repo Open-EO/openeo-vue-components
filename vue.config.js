82c2f2e06faa1c0f2314cc11a1340f4b79b79a9e
Matthias Mohr <webmaster@mamo-net.de>
2025-07-03 15:19:44 +0200

    Update SASS

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
 	publicPath: process.env.CLIENT_URL || '/',
     outputDir: './assets/',
+    css: {
+        loaderOptions: {
+            sass: {
+                api: 'modern-compiler' // Use modern Sass API
+            }
+        }
+    },
     configureWebpack: {
         devtool: 'source-map',
         entry: './dev/entry.js'
